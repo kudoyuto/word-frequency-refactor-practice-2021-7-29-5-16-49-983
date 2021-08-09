061219cb406676a9209eeffaa8ca0acf93fb0377
kudoyuto <yuto.kudo@oocl.com>
2021-08-10 00:14:31 +0800

    [wordFrequency][Yuto]refact: removed unnecessary spaces

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,22 +13,23 @@ public class WordFrequencyGame {
             return "Calculate Error";
         }
     }
-            private List<wordInfo> getInfo(String sentence) {
-             List<String> words = Arrays.asList(sentence.split(SPACE));
-             List<wordInfo> wordInfoList = new ArrayList<>();
-              for (String word: new HashSet<>(words)){
-                  int count = Collections.frequency(words,word);
-                  wordInfoList.add(new wordInfo(word,count));
-            }
-              wordInfoList.sort((firstWrdInfo, secWordInfo) -> secWordInfo.getWordCount() - firstWrdInfo.getWordCount());
-              return wordInfoList;
-            }
-            private String getConstructedWordInfo(List<wordInfo> wordInfoList){
-                return wordInfoList.stream()
-                                    .map(wordInfo ->wordInfo.getWord()+" "+wordInfo.getWordCount())
-                                    .collect(Collectors.joining("\n"));
-            }
 
+    private List<wordInfo> getInfo(String sentence) {
+        List<String> words = Arrays.asList(sentence.split(SPACE));
+        List<wordInfo> wordInfoList = new ArrayList<>();
+        for (String word : new HashSet<>(words)) {
+            int count = Collections.frequency(words, word);
+            wordInfoList.add(new wordInfo(word, count));
+        }
+        wordInfoList.sort((firstWrdInfo, secWordInfo) -> secWordInfo.getWordCount() - firstWrdInfo.getWordCount());
+        return wordInfoList;
+    }
+
+    private String getConstructedWordInfo(List<wordInfo> wordInfoList) {
+        return wordInfoList.stream()
+                .map(wordInfo -> wordInfo.getWord() + " " + wordInfo.getWordCount())
+                .collect(Collectors.joining("\n"));
+    }
 
 
 }
