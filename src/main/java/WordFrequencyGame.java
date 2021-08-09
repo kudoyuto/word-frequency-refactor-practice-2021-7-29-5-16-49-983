a3e7c56658359c4cf2b05859f20ec32c395c9e65
kudoyuto <yuto.kudo@oocl.com>
2021-08-09 23:27:59 +0800

    [wordFrequency][Yuto]refact: Implemented getInfo and getConstructedWordInfo

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,58 +5,47 @@ public class WordFrequencyGame {
 
     public static final String SPACE = "\\s+";
 
-    public String getResult(String sentence){
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(SPACE);
-                List<wordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    wordInfo wordInfo = new wordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-                //get the map for the next step of sizing the same word
-                Map<String, List<wordInfo>> map =getListMap(wordInfoList);
-
-                List<wordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<wordInfo>> entry : map.entrySet()){
-                    wordInfo wordInfo = new wordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (wordInfo wordInfo : wordInfoList) {
-                    String wordOutput = wordInfo.getValue() + " " +wordInfo.getWordCount();
-                    joiner.add(wordOutput);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
+    public String getResult(String sentence) {
+        try {
+            List<wordInfo> wordInfoList = getInfo(sentence);
+            return getConstructedWordInfo(wordInfoList);
+        } catch (Exception e) {
+            return "Calculate Error";
         }
-
-
-
-    private Map<String,List<wordInfo>> getListMap(List<wordInfo> wordInfoList) {
-        Map<String, List<wordInfo>> map = new HashMap<>();
-        for (wordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-
-
-        return map;
     }
+            private List<wordInfo> getInfo(String sentence) {
+             List<String> words = Arrays.asList(sentence.split(SPACE));
+             List<wordInfo> wordInfoList = new ArrayList<>();
+              for (String word: new HashSet<>(words)){
+                  int count = Collections.frequency(words,word);
+                  wordInfoList.add(new wordInfo(word,count));
+            }
+              wordInfoList.sort((firstWrdInfo, secWordInfo) -> secWordInfo.getWordCount() - firstWrdInfo.getWordCount());
+              return wordInfoList;
+            }
+            private String getConstructedWordInfo(List<wordInfo> wordInfoList){
+                return wordInfoList.stream()
+                                    .map(wordInfo ->wordInfo.getValue()+" "+wordInfo.getWordCount())
+                                    .collect(Collectors.joining("\n"));
+            }
+            private Map<String,List<wordInfo>> getListMap(List<wordInfo> wordInfoList) {
+                Map<String, List<wordInfo>> map = new HashMap<>();
+
+                for (wordInfo wordInfo : wordInfoList){
+                    if (!map.containsKey(wordInfo.getValue())){
+                        ArrayList arr = new ArrayList<>();
+                        arr.add(wordInfo);
+                        map.put(wordInfo.getValue(), arr);
+                    }
+
+                    else {
+                        map.get(wordInfo.getValue()).add(wordInfo);
+                    }
+                }
+
+
+                return map;
+            }
 
 
 }
